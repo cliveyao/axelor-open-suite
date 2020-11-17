23fe708fd6245ea157563a79dd35b621b7be9ceb
cliveyao <clive.yao@gmail.com>
2020-11-17 00:50:15 -0800

    test submodule commit

--- axelor-account/src/main/java/com/axelor/apps/account/db/repo/AccountAccountRepository.java
+++ axelor-account/src/main/java/com/axelor/apps/account/db/repo/AccountAccountRepository.java
@@ -15,6 +15,7 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+//test
 package com.axelor.apps.account.db.repo;
 
 import com.axelor.apps.account.db.Account;
